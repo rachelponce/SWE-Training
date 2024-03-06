1f2a9c8c639632c2dff602f3ba5ffe91562217a7
Rachel Ponce <rachel.ponce@ufl.edu>
2024-03-05 23:40:13 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// Dependency Imports
+const {ApolloServer, PubSub} = require ('apollo-server');
+const mongoose = require('mongoose');
+
+// Relative Imports
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
+
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers,
+    context: ({ req }) => ({ req, pubsub })
+});
+
+mongoose
+    .connect(MONGODB, { useNewURLParser: true })
+    .then(() => {
+        console.log('MongoDB Connected')
+        return server.listen({port: 5000});
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`)
+});

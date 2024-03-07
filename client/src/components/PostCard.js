9685d513f50828ddb883d0d6e19673b88184a5f7
Rachel Ponce <rachel.ponce@ufl.edu>
2024-03-07 06:20:40 -0500

    Complete frontend and backend

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Button, Card, Icon, Label, Image } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 
+import { AuthContext } from '../context/auth'
+import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
+import MyPopup from '../util/MyPopup'
+
 function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes }}) {
 
-    function likePost() {
-        console.log('Like post!!');
-    }
-
-    function commentOnPost() {
-        console.log("Comment on post!");
-    }
+    const { user } = useContext(AuthContext);
 
     return (
     <Card fluid>
@@ -28,26 +27,23 @@ function PostCard({ post: { body, createdAt, id, username, likeCount, commentCou
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Button as='div' labelPosition='right' onClick={likePost}>
-        <Button color='teal' basic>
-            <Icon name='heart' />
-        </Button>
-            <Label basic color='teal' pointing='left'>
-                {likeCount}
-            </Label>
-        </Button>
-
-        <Button as='div' labelPosition='right' onClick={commentOnPost}>
-        <Button color='blue' basic>
-            <Icon name='comments' />
-        </Button>
-            <Label basic color='blue' pointing='left'>
-                {commentCount}
-            </Label>
-        </Button>
+        <LikeButton user={user} post={{ id, likes, likeCount }}/>
+        <MyPopup
+          content="Comment on post"
+        >
+          <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
+            <Button color='blue' basic>
+                <Icon name='comments' />
+            </Button>
+                <Label basic color='blue' pointing='left'>
+                    {commentCount}
+                </Label>
+          </Button>
+        </MyPopup>
+        {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
-    )
+    );
 }
 
 export default PostCard;

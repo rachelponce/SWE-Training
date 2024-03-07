c16dc4ab0a20cbc6d782801d2b130b9caf0d8c6b
Rachel Ponce <rachel.ponce@ufl.edu>
2024-03-07 00:31:19 -0500

    Video 5 of Frontend Update

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { MenuMenu, MenuItem, Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
+import { AuthContext } from '../context/auth';
+
 function MenuBar() {
+  const { user, logout} = useContext(AuthContext);
   const pathname = window.location.pathname;
   
   const path = pathname === '/' ? 'home' : pathname.substring(1);
@@ -10,33 +13,49 @@ function MenuBar() {
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-    return (
-        <Menu pointing secondary size="massive" color="teal">
-          <MenuItem
-            name='home'
-            active={activeItem === 'home'}
-            onClick={handleItemClick}
-            as={Link}
-            to="/"
-          />
-          <MenuMenu position='right'>
-            <MenuItem
-                name='login'
-                active={activeItem === 'login'}
-                onClick={handleItemClick}
-                as={Link}
-                to="/login"
-            />
-            <MenuItem
-              name='register'
-              active={activeItem === 'register'}
-              onClick={handleItemClick}
-              as={Link}
-              to="/register"
-            />
-          </MenuMenu>
-        </Menu>
-    )
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="teal">
+    <MenuItem
+      name={user.username}
+      as={Link}
+      to="/"
+    />
+    <MenuMenu position='right'>
+      <MenuItem
+          name='logout'
+          onClick={logout}
+      />
+    </MenuMenu>
+  </Menu>
+  ) : (
+    <Menu pointing secondary size="massive" color="teal">
+    <MenuItem
+      name='home'
+      active={activeItem === 'home'}
+      onClick={handleItemClick}
+      as={Link}
+      to="/"
+    />
+    <MenuMenu position='right'>
+      <MenuItem
+          name='login'
+          active={activeItem === 'login'}
+          onClick={handleItemClick}
+          as={Link}
+          to="/login"
+      />
+      <MenuItem
+        name='register'
+        active={activeItem === 'register'}
+        onClick={handleItemClick}
+        as={Link}
+        to="/register"
+      />
+    </MenuMenu>
+  </Menu>
+  )
+
+  return menuBar;
 }
 
 export default MenuBar; 
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -7,6 +7,7 @@ const { argsToArgsConfig } = require('graphql/type/definition');
 module.exports = {
     Query: {
         async getPosts() {
+            console.log('getPosts');
             try {
                 const posts = await Post.find().sort({ createdAt: -1 });
                 return posts;
@@ -15,6 +16,7 @@ module.exports = {
             }
         },
         async getPost(_, { postId }) {
+            console.log('getPost');
             try {
                 const post = await Post.findById(postId);
                 if (post) {
@@ -31,9 +33,10 @@ module.exports = {
     },
     Mutation: {
         async createPost(_, { body }, context) {
+            console.log('createPosts');
             const user = checkAuth(context);
 
-            if (args.body.trim() === '') {
+            if (body.trim() === '') {
                 throw new Error('Post body must not be empty');
             }
 
@@ -53,6 +56,7 @@ module.exports = {
             return post;
         },
         async deletePost(_, { postId }, context) {
+            console.log('deletePost');
             const user = checkAuth(context);
 
             try {
